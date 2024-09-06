47c885d8d49593eb579fe450cdec114ee1b12884
henriqueacf <henrique.assis145@gmail.com>
2024-09-06 15:21:41 -0300

    feat: criando dbcontext e entidade de evento

--- PassIn/PassIn.Infrastructure/PassInDbContext.cs
+++ PassIn/PassIn.Infrastructure/PassInDbContext.cs
@@ -1,11 +1,15 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using PassIn.Infrastructure.Entities;
 
 namespace PassIn.Infrastructure;
 
 public class PassInDbContext : DbContext
 {
+    
+    public DbSet<Event> Events { get; set; }
+    
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlite("Data Source=\\<path>\\PassInDb.db");
+        optionsBuilder.UseSqlite("Data Source=C:\\Users\\Henrique\\Desktop\\PassIn-dotnet\\PassIn\\PassInDb.db");
     }
 }
