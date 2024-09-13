efd97cc4c8022d2fd2a8cbbe0b63e9efbd4ba654
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2024-09-13 15:30:18 -0300

    feat: removendo try e catch do controller de evento

--- PassIn/PassIn.Api/Controllers/EventController.cs
+++ PassIn/PassIn.Api/Controllers/EventController.cs
@@ -3,7 +3,6 @@ using PassIn.Application.UseCases.Event.GetById;
 using PassIn.Application.UseCases.Event.Register;
 using PassIn.Communication.Requests;
 using PassIn.Communication.Responses;
-using PassIn.Exceptions;
 
 namespace PassIn.Api.Controllers;
 [Route("api/[controller]")]
@@ -16,17 +15,10 @@ public class EventController : ControllerBase
     [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
     public IActionResult Register([FromBody] RequestEventJson request)
     {
-        try
-        {
-            var useCase = new RegisterEventUseCase();
-            var response = useCase.Execute(request);
+        var useCase = new RegisterEventUseCase();
+        var response = useCase.Execute(request);
 
-            return Created(string.Empty, response);
-        }
-        catch (PassInException ex)
-        {
-            return BadRequest(new ResponseErrorJson(ex.Message));
-        }
+        return Created(string.Empty, response);
     }
 
     [HttpGet]
@@ -35,16 +27,9 @@ public class EventController : ControllerBase
     [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status404NotFound)]
     public IActionResult GetById([FromRoute] Guid id)
     {
-        try
-        {
-            var useCase = new GetEventByIdUseCase();
-            var response = useCase.Execute(id);
+        var useCase = new GetEventByIdUseCase();
+        var response = useCase.Execute(id);
 
-            return Ok(response);
-        }
-        catch (PassInException ex)
-        {
-            return NotFound(new ResponseErrorJson(ex.Message));
-        }
+        return Ok(response);
     }
 }
