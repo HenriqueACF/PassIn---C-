3b7ff527a825acd80da5f2d64dd326b4573db946
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2024-09-18 14:59:41 -0300

    feat: adicionando erro na documentação do swagger

--- PassIn/PassIn.Api/Controllers/EventController.cs
+++ PassIn/PassIn.Api/Controllers/EventController.cs
@@ -39,6 +39,7 @@ public class EventController : ControllerBase
     [ProducesResponseType(typeof(ResponseRegisterJson), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status409Conflict)]
     public IActionResult Register([FromRoute] Guid eventId, [FromBody] RequestRegisterEventJson request)
     {
         var useCase = new RegisterAttendeeOnEventUseCase();
